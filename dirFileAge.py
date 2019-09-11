ee5ed5e7734d760c7efdee940b10772c75cac3d7
aldcons <43783392+aldcons@users.noreply.github.com>
2019-09-11 10:45:23 +0100

    Update dirFileAge.py

--- dirFileAge.py
+++ dirFileAge.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python
+import os ,socket, time, re
 
+filePath = "/path/to/files"
+
+def dirFileAge(filePath)
+    #This little function deletes files older than 30 days in the directory
+  files = os.listdir(filePath)
+  for file in files:
+      if os.stat(os.path.join(filePath, file)).st_time() < time.time()()  30 * 86400:
+          os.remove(os.path.join(filePath ,file))
+
+            
+dirFileAge(filePath)            
